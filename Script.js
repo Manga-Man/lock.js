e62f7c7d5f685c13d7a204702a8870a7b675b61d
Manga man <72046004+Manga-Man@users.noreply.github.com>
2021-07-12 17:57:12 -0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,35 @@
+//Javascript document
+//script protected by MIT Lisence
+//Made by Manga-man 2021
+//Enjoy!
+
+
+function setpwd(pwd) {
+    
+    
+    var Password = window.prompt("This site is password-protected \n Please insert password:  ");
+    
+    while(Password != pwd) {
+    
+   if (Password === pwd) {
+
+       alert("correct");
+   } else {
+   
+       alert("incorrect")
+       
+
+    var Password = window.prompt("This site is password-protected \n Please insert password:  ");
+    
+   }
+}
+
+}
+//while (Password != pwd) {
+//if (Password != pwd) {
+ //   document.write("<style> * { visibility: hidden;} </style>")
+//} else if (Password === pwd) {
+  //  document.write("<style> * { visibility: ;} !important</style>")
+//}
+
+//}
